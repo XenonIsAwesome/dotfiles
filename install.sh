dd07f9c17e8957418c99570035cbfa328ebc9c26
Ofek Kantargi <ofek171202@gmail.com>
2025-04-17 13:56:25 +0300

    added docker install and batcat

--- install.sh
+++ install.sh
@@ -5,6 +5,36 @@ function install_git() {
     fi
 }
 
+function install_docker() {
+    if ! command -v docker &> /dev/null; then
+        # Add Docker's official GPG key:
+        sudo apt update -y
+        sudo apt install -y ca-certificates curl
+        sudo install -m 0755 -d /etc/apt/keyrings
+        sudo curl -fsSL https://download.docker.com/linux/ubuntu/gpg -o /etc/apt/keyrings/docker.asc
+        sudo chmod a+r /etc/apt/keyrings/docker.asc
+
+        # Add the repository to Apt sources:
+        echo \
+        "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.asc] https://download.docker.com/linux/ubuntu \
+        $(. /etc/os-release && echo "${UBUNTU_CODENAME:-$VERSION_CODENAME}") stable" | \
+        sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+        sudo apt update -y
+
+        # Install docker debian packages
+        sudo apt install -y docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
+    fi
+}
+
+function install_misc() {
+    sudo apt install -y bat
+}
+
+function install_all() {
+    install_docker;
+    install_misc;
+}
+
 function clone_dotfiles() {
     install_git;
     if [ ! -d "${HOME}/.dotfiles" ]; then
